b8406f01894bfcbf7a2362dcfad485ac65ca47c3
Rafy Amgad Benjamin <rafy.amgadbenjamin@gmail.com>
2019-02-23 22:53:40 +0200

    ICPC after finishing the problem and testing the sample cases

--- 4873 - Penney Game/Program.cs
+++ 4873 - Penney Game/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _4873___Penney_Game
 {
@@ -10,6 +6,47 @@ namespace _4873___Penney_Game
     {
         static void Main(string[] args)
         {
+            int totalTrialNo = int.Parse(Console.ReadLine());
+            int[] trailNo = new int[totalTrialNo];
+            string[] trials = new string[totalTrialNo];
+//Getting the input from the user in the correct format 
+            for (int i = 0; i < totalTrialNo; i++)
+            {
+                trailNo[i] = int.Parse(Console.ReadLine());
+                trials[i] = Console.ReadLine();
+            }
+            for (int i = 0; i < totalTrialNo; i++)
+            {
+                Console.WriteLine(PlayHeadTail(trials[i], trailNo[i].ToString()));
+            }
+        }
+        public static string PlayHeadTail(string trials, string trailNo)
+        {
+            //TTT, TTH, THT, THH, HTT, HTH, HHT and
+            // HHH.
+
+            string[] probability = new string[8];
+            int[] probabilityValues = new int[8];
+            probability[0] = "TTT";
+            probability[1] = "TTH";
+            probability[2] = "THT";
+            probability[3] = "THH";
+            probability[4] = "HTT";
+            probability[5] = "HTH";
+            probability[6] = "HHT";
+            probability[7] = "HHH";
+
+            for (int i = 0; i <= trials.Length - 3; i++)
+            {
+                string temp = trials.Substring(i, 3);
+                //  Array.Find(probability,p => p ==temp);
+                probabilityValues[Array.IndexOf(probability, temp)]++;
+
+            }
+
+
+            Array.ForEach(probabilityValues, v => { trailNo += " "; trailNo += v; });
+            return trailNo;
         }
     }
 }
